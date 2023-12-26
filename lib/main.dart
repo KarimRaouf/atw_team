0b700914d22d7f9983bbf3a2718e5fa99d841b9a
MaryamAfify <maryamafify51@gmail.com>
2023-12-26 12:49:14 +0200

    theme data for all app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+        statusBarColor: Colors.black,
+        statusBarIconBrightness: Brightness.light),
+  );
   runApp(const MainApp());
 }
 
@@ -16,6 +23,18 @@ class MainApp extends StatelessWidget {
           cursorColor: Colors.white, // Set your desired cursor color here
         ),
         useMaterial3: true,
+        textTheme: GoogleFonts.spaceGroteskTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          titleSpacing: 0,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          elevation: 0,
+        ),
       ),
       home: const Scaffold(
         body: Center(
